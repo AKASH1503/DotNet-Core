c7b5a64c2ea3fa3686d2f4b564c5dbed74428eef
AKASH1503 <rasalakash8@gmail.com>
2021-01-15 20:14:56 +0530

    MVC Setup
    	1. call services.AddMvc() in ConfigureServices.
    	2. Then add mvc middleware

--- SampleProject/Startup.cs
+++ SampleProject/Startup.cs
@@ -23,6 +23,7 @@ namespace SampleProject
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc(options => options.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,22 +43,24 @@ namespace SampleProject
             //options.DefaultFileNames.Add("default.html");
             //app.UseDefaultFiles(options);
             //app.UseStaticFiles();
-            FileServerOptions fileServerOptions = new FileServerOptions();
-            fileServerOptions.DefaultFilesOptions.DefaultFileNames.Clear();
-            fileServerOptions.DefaultFilesOptions.DefaultFileNames.Add("default1.html");
-            app.UseFileServer(fileServerOptions);
+            //FileServerOptions fileServerOptions = new FileServerOptions();
+            //fileServerOptions.DefaultFilesOptions.DefaultFileNames.Clear();
+            //fileServerOptions.DefaultFilesOptions.DefaultFileNames.Add("default.html");
+            //app.UseFileServer(fileServerOptions);
 
             app.UseRouting();
             
             app.UseStaticFiles();
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    throw new Exception("Somwthing went wrong");
-                    await context.Response.WriteAsync("Hello From 1st MiddleWare");
-                });
-            });
+            //Calling after UseStaticFiles
+            app.UseMvcWithDefaultRoute();
+            
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapGet("/", async context =>
+            //    {
+            //        await context.Response.WriteAsync("Hello World");
+            //    });
+            //});
         }
     }
 }
