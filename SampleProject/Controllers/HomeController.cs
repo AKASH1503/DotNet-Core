7ee0664ad30a6180bc86da621ee250343a74e25e
AKASH1503 <rasalakash8@gmail.com>
2021-01-15 20:27:21 +0530

    AddMvc vs ADDMvcCore
    	1. ADDMvcCore only add core MVC services
    		AddMvc add all required MVC
    		AddMvc method calls ADDMvcCore method internally

---
+++ SampleProject/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SampleProject.Controller
+{
+    public class HomeController : ControllerBase
+    {
+        public JsonResult Index()
+        {
+            return new JsonResult(new { id = 1, Name = "Akash" });
+        }
+    }
+}
