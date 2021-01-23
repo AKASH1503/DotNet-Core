47426f63d569e0734b1df51c0eb9a12529b85577
AKASH1503 <rasalakash8@gmail.com>
2021-01-23 09:35:49 +0530

    Routing (Attribute Routing)
    	1. Convensional
    	2. Attribute Routing
    		- with attribute rouing [Route] attribute is used to define the routs
    		- Rout attribute can be applied on the Controller or the Controller Action method
    		- routs are placed next to the action methods

--- SampleProject/Controllers/HomeController.cs
+++ SampleProject/Controllers/HomeController.cs
@@ -8,6 +8,8 @@ using SampleProject.ViewModels;
 
 namespace SampleProject.Controllers
 {
+    //[Route("Home")]
+    [Route("[controller]/[action]")]
     public class HomeController : Controller
     {
         private readonly IEmployeeRepository _employeeRepository;
@@ -20,16 +22,26 @@ namespace SampleProject.Controllers
         {
             _employeeRepository = employeeRepository;
         }
+
+        [Route("")]
+        //[Route("Index")]
+        //[Route("[action]")]
+        [Route("~/")]
         public ViewResult Index()
         {
-            var model= _employeeRepository.GetAllEmployee(); ;
+            var model = _employeeRepository.GetAllEmployee(); ;
             return View(model);
+        
         }
-        public ViewResult Details(int id)
+
+        //[Route("Details/{id?}")]
+        //[Route("[action]/{id?}")]
+        [Route("{id?}")]
+        public ViewResult Details(int? id)
         {
             HomeDetailsViewModel homeDetailsViewModel = new HomeDetailsViewModel()
             {
-                employee = _employeeRepository.GetEmployee(1),
+                employee = _employeeRepository.GetEmployee(id??1),
                 PageTitle = "Employee Details"
             };
             return View(homeDetailsViewModel);
