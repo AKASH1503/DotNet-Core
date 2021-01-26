c4ec4fa654ea7ddcfa08491a9f53c99f5221928e
AKASH1503 <rasalakash8@gmail.com>
2021-01-26 22:46:43 +0530

    AddScoped
    - With a scoped service we get the same instance within the scope of a given http request but a new instance accross different http requests.

--- SampleProject/Startup.cs
+++ SampleProject/Startup.cs
@@ -26,7 +26,7 @@ namespace SampleProject
         {
             services.AddMvc(options => options.EnableEndpointRouting = false);
             //services.AddMvcCore(options => options.EnableEndpointRouting = false);
-            services.AddSingleton<IEmployeeRepository, MockEmployeeRepository>();
+            services.AddScoped<IEmployeeRepository, MockEmployeeRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
