3c66ef2184ce33a8ebacffb6cb179a5c058d2104
AKASH1503 <rasalakash8@gmail.com>
2021-01-27 12:48:10 +0530

    Keeping Domain Modles and Database schem,a in sync
    - USe Migrations to keep domain models and database schema in sync
    	- to add a new migrations use Add-Migration comand
    	- To update the database with the latest migration use Update-Database command
    	- to remove latest migration that not yet applied to the database use Remove-Migration
    	- _EFMigrationsHistory table is used to keep track of the migrations that are applied to the database
    	- ModelSnapShot.cs file contains the snapshot of the current model and is used to determine what has changed when
    		adding the next migration.
    	- To remove migration -
    		- Update-Database
    		- Remove-Migration

--- SampleProject/Migrations/20210127071309_addPhoPathColumn.Designer.cs
+++ SampleProject/Migrations/20210127071309_addPhoPathColumn.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SampleProject.Models;
 
 namespace SampleProject.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210127071309_addPhoPathColumn")]
+    partial class addPhoPathColumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
